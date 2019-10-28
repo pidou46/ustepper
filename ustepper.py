3f6965b58df1cf60866d46d1eab7b1bbfa30598a
pidou46 <pidou46@laposte.net>
2019-10-28 18:29:47 +0100

    try with micropython.schedule()

--- ustepper.py
+++ ustepper.py
@@ -1,6 +1,9 @@
 # drv8825 driver
 from machine import Pin, PWM, Timer
+import micropython, sys
 import time
+
+micropython.alloc_emergency_exception_buf(100)
 
 class Stepper:
     def __init__(self, dirPin, stepPin, msPin):
@@ -8,6 +11,7 @@ class Stepper:
         self.stepPin=PWM(stepPin, freq=0, duty=0)
         self.msPin=msPin #microstep pin M0
         #M0,M1,M2,ms
+        self.PrepNextRun_ref=self.PrepNextRun
         self.timeTable=[(1,0,0,2000),(0,0,0,20000),(1,0,0,2000)]
         self.tim=Timer(-1)
         self.freq=1000
@@ -23,28 +27,41 @@ class Stepper:
         #    self.timeTable[]int(steps/self.freq)*1000)
         pass
 
-    #integre une compensation du temps du retard moyen du Timer (1_ms)
-    def Run(self, t):
+    def FirstRun(self):
+        print("FirstRun: %s"%time.ticks_us())
         self.stepPin.duty(32)
+        self.PrepNextRun(0)
+        self.Run(0)
+
+    def PrepNextRun(self,t):
+        print("PrepNextRun: %s"%time.ticks_us())
         try:
-            print(time.ticks_us())
-            self.msPin=self.timeTable[self.index][0]
-            self.tim.init(period=(self.timeTable[self.index][3])-1, mode=Timer.ONE_SHOT, callback=self.Run)
+            self.ms=self.timeTable[self.index][0]
+            self.p=self.timeTable[self.index][3]
             self.index+=1
         except:
             self.stepPin.duty(0)
+            print("Motor have been stopped...")
+            self.tim.deinit()
+            
+
+    #cela fonctionne masi devrait etre optimiser : http://docs.micropython.org/en/latest/reference/isr_rules.html
+    def Run(self, t):
+        self.msPin=self.ms
+        self.tim.init(period=self.p, mode=Timer.ONE_SHOT, callback=self.Run)
+        micropython.schedule(self.PrepNextRun_ref,0)
 
 if __name__ == '__main__':
     test=Stepper(Pin(0, Pin.OUT),Pin(2),Pin(3))
-    test.Run(0)
+    test.FirstRun()
     while True:
         print('meanwhile...')
         time.sleep(1)
 
-#290696176	
-#292696529	2000353
-#312696887	20000358
-#314697197	2000310
+#290696176  
+#292696529  2000353
+#312696887  20000358
+#314697197  2000310
 #avec une compensation de 1_ms la deviation est d'environ:
 #300_us a chaque changement de palier
         
