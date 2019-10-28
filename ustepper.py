7f642104a1099b0be3a3f1980755266e89512dc8
pidou46 <pidou46@laposte.net>
2019-10-28 17:03:29 +0100

    Update ustepper.py

--- ustepper.py
+++ ustepper.py
@@ -18,25 +18,33 @@ class Stepper:
         #converti les steps en millisecondes en tenant compte
         #cree une rampe d'acceleration / deceleration par paliers
         #en utilisant les microsteps
-        #integre une compensation du temps du retard moyen du Timer (1_ms)
         
         #for i in  
         #    self.timeTable[]int(steps/self.freq)*1000)
         pass
-    
+
+    #integre une compensation du temps du retard moyen du Timer (1_ms)
     def Run(self, t):
         self.stepPin.duty(32)
         try:
             print(time.ticks_ms())
             self.msPin=self.timeTable[self.index][0]
-            self.tim.init(period=self.timeTable[self.index][3], mode=Timer.ONE_SHOT, callback=self.Run)
+            self.tim.init(period=(self.timeTable[self.index][3])-1, mode=Timer.ONE_SHOT, callback=self.Run)
             self.index+=1
         except:
             self.stepPin.duty(0)
 
 if __name__ == '__main__':
     test=Stepper(Pin(0, Pin.OUT),Pin(2),Pin(3))
-    test.Spin(0)
+    test.Run(0)
     while True:
         print('meanwhile...')
         time.sleep(1)
+
+#290696176	
+#292696529	2000353
+#312696887	20000358
+#314697197	2000310
+#avec une compensation de 1_ms la deviation est d'environ:
+#300_us a chaque changement de palier
+        
